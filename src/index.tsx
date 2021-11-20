40dcfa61e8479aa4932f72f21ab6f85c82987665
Rillai <qiwi06@list.ru>
2021-11-21 02:20:06 +0400

    Добавил типизацию, добавил ui-библиотеку, исправил версию реакт-роутер-дом(на новой версии нет поддержки типов) доработал структуру, добавил eslintrc и tsconfig.json

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from './app/App';
+import './app/styles/index.css';
+import { App } from './app/app';
+import firebase from 'firebase/compat';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const Context = createContext({});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Context.Provider value={{
+      firebase,
+      auth,
+      firestore,
+    }}>
+      <App />
+    </Context.Provider>,
+    document.getElementById('root'),
 );
